a0e50c9afa615e35d032174fd075fac8cb868d9c
DSRN0000 <dsrn0000@gmail.com>
2023-11-15 17:36:28 +0900

    feat(BonusMenuTest): 보너스 메뉴 테스트 구현

---
+++ src/test/java/christmas/test/BonusMenuTest.java
@@ -0,0 +1,27 @@
+package christmas.test;
+
+import christmas.discount.BonusMenu;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusMenuTest {
+
+    @Test
+    public void testBonusMenuWithHighPrice() {
+        BonusMenu bonusMenu = new BonusMenu();
+        bonusMenu.bonusMenu(130000);
+
+        assertEquals("삼페인 1개", BonusMenu.getBonusMenu());
+        assertEquals(25000, BonusMenu.getBonusMenuPrice());
+    }
+
+    @Test
+    public void testBonusMenuWithLowPrice() {
+        BonusMenu bonusMenu = new BonusMenu();
+        bonusMenu.bonusMenu(100000);
+
+        assertEquals("없음", BonusMenu.getBonusMenu());
+        assertEquals(0, BonusMenu.getBonusMenuPrice());
+    }
+}
