b5ba1d0e1a7f6142096b6644fa442b73812acd42
DSRN0000 <dsrn0000@gmail.com>
2023-11-15 11:46:35 +0900

    feat(SpecialDay): 스페셜 데이 enum 구현

---
+++ src/main/java/christmas/day/SpecialDay.java
@@ -0,0 +1,11 @@
+package christmas.day;
+
+public enum SpecialDay {
+    THREE(3),TEN(10),SEVENTEEN(17),TWENTY_FOUR(24),TWENTY_FIVE(25),THIRTY_ONE(31);
+
+    private final int specialDay;
+
+    SpecialDay(int specialDay){
+        this.specialDay = specialDay;
+    }
+}
