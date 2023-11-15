3c7d5c156881411198ff2cbf13c114fbb2dfed03
DSRN0000 <dsrn0000@gmail.com>
2023-11-15 17:39:41 +0900

    feat(FormatNumberTest): 포멧넘버 테스트 구현

---
+++ src/test/java/christmas/test/FormatNumberTest.java
@@ -0,0 +1,26 @@
+package christmas.test;
+
+import christmas.view.FormatNumber;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FormatNumberTest {
+
+    @Test
+    public void testFormatNumber() {
+        FormatNumber formatNumber = new FormatNumber();
+
+        int positiveNumber = 123456789;
+        String expectedFormattedNumber = "123,456,789";
+        assertEquals(expectedFormattedNumber, formatNumber.formatNumber(positiveNumber));
+
+        int negativeNumber = -987654321;
+        expectedFormattedNumber = "-987,654,321";
+        assertEquals(expectedFormattedNumber, formatNumber.formatNumber(negativeNumber));
+
+        int zero = 0;
+        expectedFormattedNumber = "0";
+        assertEquals(expectedFormattedNumber, formatNumber.formatNumber(zero));
+    }
+}
