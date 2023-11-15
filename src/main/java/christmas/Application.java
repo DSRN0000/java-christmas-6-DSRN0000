7ee4ad6eee73d3b1357477e9915a04248f92770b
DSRN0000 <dsrn0000@gmail.com>
2023-11-15 12:04:01 +0900

    feat(Application): Application Test 테스트에 대한 Application 코드 수정

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,15 @@
 package christmas;
 
+import christmas.controller.MainController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try{
+            MainController mainController = new MainController();
+            mainController.run();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
