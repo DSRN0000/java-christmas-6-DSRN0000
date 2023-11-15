d1288f376e21625f930092facb40431821c536d0
DSRN0000 <dsrn0000@gmail.com>
2023-11-15 11:47:07 +0900

    feat(Weekday): 평일 enum 구현

---
+++ src/main/java/christmas/day/Weekday.java
@@ -0,0 +1,11 @@
+package christmas.day;
+
+public enum Weekday {
+    SUNDAY("sunday"),MONDAY("monday"),TUESDAY("tuesday"),WEDNESDAY("wednesday"),THURSDAY("thursday");
+
+    private final String weekday;
+
+    Weekday(String weekday){
+        this.weekday = weekday;
+    }
+}
