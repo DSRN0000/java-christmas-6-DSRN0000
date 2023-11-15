b2ddcfbcad7ff11687c3ceac96278606446e3828
DSRN0000 <dsrn0000@gmail.com>
2023-11-15 11:47:34 +0900

    feat(Weekend): 주말 enum 구현

---
+++ src/main/java/christmas/day/Weekend.java
@@ -0,0 +1,11 @@
+package christmas.day;
+
+public enum Weekend {
+    FRIDAY("friday"), SATURDAY("saturday");
+
+
+    private final String weekend;
+    Weekend(String weekend){
+        this.weekend = weekend;
+    }
+}
